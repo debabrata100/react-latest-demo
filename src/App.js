0ca23a885e9b0cff78ddc51ccd86698cf5cf9dbe
Debabrata Nayak <debabrata@Debabratas-MacBook-Air.local>
2019-11-07 12:51:44 +0530

    editorconfig test

--- src/App.js
+++ src/App.js
@@ -1,43 +1,63 @@
-import React, { Suspense, lazy } from 'react';
-import './App.css';
-import { connect } from 'react-redux';
-import { makeSelectUser } from './selectors';
-import * as actionTypes from './constants'; 
+import React, { Suspense, lazy } from "react";
+import "./App.css";
+import { connect } from "react-redux";
+import { makeSelectUser } from "./selectors";
+import * as actionTypes from "./constants";
 import { Title, PriceTag, Actions, Button } from "./ui";
 // import Pinkman from "./PinkmanLazyComponent";
 
-const Pinkman = lazy(()=> import ('./PinkmanLazyComponent'))
+const Pinkman = lazy(() => import("./PinkmanLazyComponent"));
 
-
-function App({ quantity, loading, onQuantityUp, onQuantityDown, onQuantityAsyncUp, onQuantityAyncDown, userName }) {
-  return (
-    <div className="App">
-        <Title>Seller: {userName}</Title>
-        { 
-            quantity > 0 && 
-            <Suspense fallback={<div>Loading...</div>}>
-              <Pinkman count={quantity} />
-            </Suspense>
-        }
-        <PriceTag>Methamphetamine Quantity: $ {quantity} pound</PriceTag>
-        <Actions>
-            <Button onClick={onQuantityUp} disabled={loading}>UP</Button>
-            <Button onClick={onQuantityAsyncUp} disabled={loading}>{loading ? 'loading...' : 'ASYNC UP'}</Button>
-            <Button onClick={onQuantityDown} disabled={loading}>DOWN</Button>
-            <Button onClick={onQuantityAyncDown} disabled={loading}>{loading ? 'loading...' : 'ASYNC DOWN'}</Button>
-        </Actions>
-    </div>
-  );
+function App({
+    quantity,
+    loading,
+    onQuantityUp,
+    onQuantityDown,
+    onQuantityAsyncUp,
+    onQuantityAyncDown,
+    userName
+}) {
+    return (
+        <div className="App">
+            <Title>Seller: {userName}</Title>
+            {quantity > 0 && (
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Pinkman count={quantity} />
+                </Suspense>
+            )}
+            <PriceTag>Methamphetamine Quantity: $ {quantity} pound</PriceTag>
+            <Actions>
+                <Button onClick={onQuantityUp} disabled={loading}>
+                    UP
+                </Button>
+                <Button onClick={onQuantityAsyncUp} disabled={loading}>
+                    {loading ? "loading..." : "ASYNC UP"}
+                </Button>
+                <Button onClick={onQuantityDown} disabled={loading}>
+                    DOWN
+                </Button>
+                <Button onClick={onQuantityAyncDown} disabled={loading}>
+                    {loading ? "loading..." : "ASYNC DOWN"}
+                </Button>
+            </Actions>
+        </div>
+    );
 }
 
-const mapStateToProps = state =>({
-  ...state.appReducer,
-  userName: makeSelectUser(state.appReducer)
-})
+const mapStateToProps = state => ({
+    ...state.appReducer,
+    userName: makeSelectUser(state.appReducer)
+});
 const mapDispatchToProps = dispatch => ({
-    onQuantityAsyncUp: () => dispatch({ type: actionTypes.QUANTITY_ASYNC_UP, value: 1 }),
+    onQuantityAsyncUp: () =>
+        dispatch({ type: actionTypes.QUANTITY_ASYNC_UP, value: 1 }),
     onQuantityUp: () => dispatch({ type: actionTypes.QUANTITY_UP, value: 1 }),
-    onQuantityDown: () => dispatch({ type: actionTypes.QUANTITY_DOWN, value: 1 }),
-    onQuantityAyncDown: () => dispatch({ type: actionTypes.QUANTITY_ASYNC_DOWN, value: 1 }),
-})
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+    onQuantityDown: () =>
+        dispatch({ type: actionTypes.QUANTITY_DOWN, value: 1 }),
+    onQuantityAyncDown: () =>
+        dispatch({ type: actionTypes.QUANTITY_ASYNC_DOWN, value: 1 })
+});
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(App);
--- src/staticReducers/about.js
+++ src/staticReducers/about.js
@@ -1,11 +1,11 @@
 const initialState = {
     about: `What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took a galley of type and scrambled it to make a type specimen book it has?`
-}
+};
 
 const reducer = (state = initialState, action) => {
     switch (action) {
         default:
-          return state;
-      }
-}
+            return state;
+    }
+};
 export default reducer;
